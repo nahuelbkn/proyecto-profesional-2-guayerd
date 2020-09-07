264d67a7c498eb5e3dcdc93699083b18e1e3192e
Guido <alferezguido@outlook.com>
2020-09-07 19:21:26 -0300

    Cancelar Acciones

--- respuestaEmail.js
+++
@@ -1,7 +0,0 @@
-// INICIO
-
-// LE HACEMOS LA PREGUNTA AL USUARIO
-let respuesta = prompt("¿Desea obtener novedades en su email?");
-
-// ALMACENAMOS EN STORAGE ESA RESPUESTA
-localStorage.setItem("respuesta",respuesta);
