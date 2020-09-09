b8ab66fca12d57b8435a1c95b1ade1fd858c8b71
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-09-09 14:34:34 -0300

    hasNumber() -> Se desarrolla explicación

--- js/validacion.js
+++ js/validacion.js
@@ -160,8 +160,12 @@ function validarMensaje(elemento)
     return respuesta;
 }
 
-function hasNumber(myString) 
+function hasNumber(string) 
 {
-    return /\d/.test(myString);
+    let rta = [0-9].test(string); // let rta = /\d/.test(string);
+    return rta;
+    // \d: Busca cualquier dígito (número arábigo). Equivalente a [0-9].
+    //Por ejemplo, /\d/ o /[0-9]/ encuentra el "2" en "string" (string = "B2 es el número de suite").
+    // https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions/Clases_de_caracteres
 }
   
