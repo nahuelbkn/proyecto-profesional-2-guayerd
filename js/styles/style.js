61936096f8b65ef56260ac36bee415788b615ff7
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-10-17 02:41:39 -0300

    Prueba estilos H1

--- js/styles/style.js
+++ js/styles/style.js
@@ -7,3 +7,23 @@ function animar()
 }
 
 setInterval(animar, 5000);
+
+
+
+/* PRUEBAS: Zona de pruebas -> reordenar el caso de adoptarse -----------------------------------------*/
+
+anime.timeline({loop: 2})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: Infinity,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
